5ba9bf70b475295f3c3f435b3c166a29a1cd4194
aliahanch <38158497+aliahanch@users.noreply.github.com>
2020-06-02 20:18:40 +0430

    Update Start.js

--- src/Start/Start.js
+++ src/Start/Start.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import './styleStart.css';
-import {Ring} from 'react-spinners-css';
+import {Spinner} from 'react-spinners-css';
 
 
 class Start extends Component {
     render() {
         return (
             <div className="Ali">
-                <p className="spin"><Ring color="#50AE65" size={50}/>
+                <p className="spin"><Spinner color="#50AE65" size={50}/>
                 </p>
             </div>
         )}
