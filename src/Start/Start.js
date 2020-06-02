f522181bafc9f060dbeb6a5557fe65ec0e83efe2
aliahanch <38158497+aliahanch@users.noreply.github.com>
2020-06-02 20:37:47 +0430

    Update Start.js

--- src/Start/Start.js
+++ src/Start/Start.js
@@ -7,7 +7,7 @@ class Start extends Component {
     render() {
         return (
             <div className="Ali">
-                <p className="spin"><Spinner color="#50AE65" size={50}/>
+                <p className="spin"><Spinner color="#343334" size={50}/>
                 </p>
             </div>
         )}
